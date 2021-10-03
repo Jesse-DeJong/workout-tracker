2a70a9d74d2a8d650946e913928851e4dbb459fd
Jesse De Jong <jdoceau@gmail.com>
2021-10-03 12:45:25 +1100

    Created mongoose schema

---
+++ Develop/models/workout.js
@@ -0,0 +1,48 @@
+// Import Mongoose
+const mongoose = require("mongoose");
+
+// Mongoose Schema
+const Schema = mongoose.Schema;
+
+// Create Workout Schema
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Please enter an exercise type."
+            },
+            name: {
+                type: String,
+                required: "Please name this exercise."
+            },
+            duration: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ]
+});
+
+// Initialise Schema
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+// Export Schema
+module.exports = Workout;
