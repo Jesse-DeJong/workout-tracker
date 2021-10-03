6c0d9352d178f4b5a1462e72fc41fb15d57abb5e
Jesse De Jong <jdoceau@gmail.com>
2021-10-03 15:16:17 +1100

    fixed line chart

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -45,19 +45,19 @@ router.get("/workouts/range", async (req, res) => {
 });
 
 // POST new workout
-router.post("/workouts", (req, res) => {
+router.post("/workouts", async (req, res) => {
     
     try {
-        db.Workout.create(req.body);
+        const data = await db.Workout.create(req.body);
         res.status(200).json(data);
     } catch (err) {
         res.status(500).json(err);
     }
 })
 
+// PUT update route
 router.put("/workouts/:id", async (req, res) => {
-    console.log(req.body);
-    console.log(req.params.id);
+
     try {
         const data = await db.Workout.findByIdAndUpdate(
             { _id: req.params.id },
